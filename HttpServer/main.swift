7a48dad54f4559d00f8804df638404a8a49cce30
Alexandr Koksharov <alexandr.koksharov@oracle.com>
2018-12-01 17:58:39 +0100

    Initial Commit

---
+++ HttpServer/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HttpServer
+//
+//  Created by Koksharov Alexandr on 01/12/2018.
+//  Copyright © 2018 Koksharov Alexandr. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
